0930b5f7da0a02fc210d81c93393f36e6a565860
chrisjimenez <chrisajim@gmail.com>
2015-04-12 01:15:38 -0400

    Cleaned up some code. Cleaned up dir.

---
+++ app/js/app.js
@@ -0,0 +1,60 @@
+/*
+██████╗  ██████╗ ██████╗  ██████╗ ██████╗ ██████╗ ███╗   ██╗██████╗ ███████╗
+██╔══██╗██╔═══██╗██╔══██╗██╔════╝██╔═══██╗██╔══██╗████╗  ██║██╔══██╗╚══███╔╝
+██████╔╝██║   ██║██████╔╝██║     ██║   ██║██████╔╝██╔██╗ ██║██████╔╝  ███╔╝ 
+██╔═══╝ ██║   ██║██╔═══╝ ██║     ██║   ██║██╔══██╗██║╚██╗██║██╔══██╗ ███╔╝  
+██║     ╚██████╔╝██║     ╚██████╗╚██████╔╝██║  ██║██║ ╚████║██║  ██║███████╗
+╚═╝      ╚═════╝ ╚═╝      ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝
+
+By: Chris Jimenez & Yoland                                                                            
+app.js
+*/
+
+'use strict';
+
+
+/**
+*  Run when DOM is ready.
+*/
+$(document).ready(function(){
+
+
+
+});
+
+
+}
+
+
+/**
+* Displays the Interested Courses List on the left
+*/
+function displayInterestedCourses(){
+
+}
+
+/**
+* Displays the Interested Courses List on the left
+*/
+function displayCurrentCourses(){
+
+}
+
+/**
+* Displays the Top Courses List on top of the course catalog.
+*/
+function displayTopCourses(){
+
+}
+
+/**
+* Displays the Recommended Courses List on top of the course catalog.
+*/
+function displayRecommendedCourses(){
+
+}
+
+
+function displayCourseCatalog(){
+
+}
